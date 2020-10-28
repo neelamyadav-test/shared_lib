cebb8793ab058cae2cc766b73e15e3654301fd39
Neelam Yadav <neelam.yadav@nagarro.com>
2020-10-28 16:58:34 +0530

    sample shared lib

---
+++ src/com/example/GlobalVars.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+package com.example
+
+class GlobalVars {
+   static String foo = "bar"
+}
---
+++ vars/sayHello.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def call(String name = 'all') {
+  echo "Hello, ${name}."
+}
+
